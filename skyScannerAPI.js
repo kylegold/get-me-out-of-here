82f4e5e1be914cce0dcbe79210bdb7ae00e2aa11
rrrossettiii <rrrossettiii@gmail.com>
2020-10-13 13:55:37 -0400

    Create skyScannerAPI.js

---
+++ skyScannerAPI.js
@@ -0,0 +1,32 @@
+// Get to SkyScanner
+// 
+
+// Header Parameters
+// - 
+
+// Required Parameters*
+// - countryString var;
+// - currency var ;
+// - locale = 'en-US';
+// - originPlace;
+// - destinationPlace;
+// - departureDate = [“yyyy-mm-dd”, “yyyy-mm” or “anytime”];
+
+// optional parameters*
+// - returnDate “yyyy-mm-dd”, “yyyy-mm” or “anytime”;
+// - shortApiKey 'ra66933236979928';
+
+// 
+
+// 
+
+// 
+
+// List Places
+// Get a list of places that match a query string.
+
+// Required Parameters*
+// - queryString = 'Stockholm';
+// - countryString = 'UK'; // - where is your user*
+// - currencyString = 'GDP'
+// - locale = 'en-US'
