bb19509d7599c2e54bf3ba4d037802f2ddafe1b0
rrrossettiii <rrrossettiii@gmail.com>
2020-10-14 15:48:09 -0400

    reorganized

--- formTest/covidAPI.js
+++ formTest/covidAPI.js
@@ -1,37 +1,62 @@
-// Dependencies / DOM Elements
-var userChoice;
+// loop - for every 'data' entry...;
+for (i = 0; i < statesList.length; i++) {
+  // - create option tag; apply the 'province' to the value of <option>; append option to select '.stateDropdown';
+  var provinceOption = $("<option></option>");
+  provinceOption.attr("value", statesList[i].name).text(statesList[i].name);
+  $("#userState").append(provinceOption);
+}
 
-// Create an onclick once user enters information into form
-$("#submit-btn").on("click", function (event) {
-  userChoice = $("#userInput").val();
-  userChoice = "";
-  console.log(userChoice);
-
-  //   var searchQuery =
-  //     "https://covid-19-statistics.p.rapidapi.com/provinces?iso=" + userChoice;
-  //   console.log(searchQuery);
-
-  var queryUrl =
-    "https://rapidapi.p.rapidapi.com/reports?iso=USA&region_name=US&city_name=" +
-    userChoice +
-    "&date=2020-04-16";
-  console.log(queryUrl);
+$("#userState").on("change", function () {
+  event.stopPropagation();
+  $("#userCity").children().remove().end();
+  userState = $("#userState").val();
+  console.log(userState);
 
   const settings = {
     async: true,
     crossDomain: true,
-    url: queryUrl,
+    url:
+      "https://rapidapi.p.rapidapi.com/reports?region_province=" +
+      encodeURIComponent(userState),
     method: "GET",
     headers: {
       "x-rapidapi-host": "covid-19-statistics.p.rapidapi.com",
-      "x-rapidapi-key": "07058048ffmshe16787ad7b4eeffp1c88f9jsn4040b743a04a",
+      "x-rapidapi-key":
+        "07058048ffmshe16787ad7b4eeffp1c88f9jsn4040b743a04a",
     },
   };
+  console.log(settings.url);
   $.ajax(settings).done(function (response) {
-    console.log(response);
-    testResults = response;
+    citiesData = response.data[0].region.cities;
+    console.log(citiesData);
+    for (i = 0; i < citiesData.length; i++) {
+      var cityOption = $("<option></option>");
+      cityOption.attr("value", citiesData[i].name).text(citiesData[i].name);
+      $(".cityDropdown").append(cityOption);
+    }
+    $("#submit-btn").off("click");
+    $("#submit-btn").on("click", function (event) {
+      $("#reportElement").remove().end();
+      event.stopPropagation();
+      userCityInput = $("#userCity").val();
+      for (i = 0; i < citiesData.length; i++) {
+        if (citiesData[i].name == userCityInput) {
+          console.log("works");
+          console.log(citiesData[i]);
+          console.log(i);
+          userReport = citiesData[i];
+          report = $("<h1>")
+            .text(
+              userReport.name + " confirmed cases: " + userReport.confirmed
+            )
+            .attr("id", "reportElement");
+          $("body").append(report);
+        }
+      }
+    });
   });
 });
+
 // Display city into DOM
 
 // Open Covid AJAX API and compare to location
