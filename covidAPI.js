6eba354392017a519f472415b85a7f885d004023
jordanwhunter <jordanwhunter.mail@gmail.com>
2020-10-13 22:41:58 -0400

    psuedo code for covid api

--- covidAPI.js
+++ covidAPI.js
@@ -1,1 +1,9 @@
-// Test text
+// Dependencies / DOM Elements
+
+// Create an onclick once user enters information into form
+
+// Display city into DOM
+
+// Open Covid AJAX API and compare to location
+
+// User looks at returned results and 
