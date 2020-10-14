a142ece3515e9efaaf12210accd8ab66fba8f7b7
jordanwhunter <jordanwhunter.mail@gmail.com>
2020-10-14 12:53:11 -0400

    Pushing to add updates to covidAPI

--- formTest/covidAPI.js
+++ formTest/covidAPI.js
@@ -22,18 +22,267 @@ var userChoice;
 //             console.log(response);
 //             testResults = response;
 //         });
-$("#submit-btn").on("click", function (event) {
-  userChoice = $("#userInput").val();
-  userChoice = "";
-  console.log(userChoice);
+var statesList = [
+            {
+                name: "Alabama",
+                abbreviation: "AL",
+            },
+            {
+                name: "Alaska",
+                abbreviation: "AK",
+            },
+            {
+                name: "American Samoa",
+                abbreviation: "AS",
+            },
+            {
+                name: "Arizona",
+                abbreviation: "AZ",
+            },
+            {
+                name: "Arkansas",
+                abbreviation: "AR",
+            },
+            {
+                name: "California",
+                abbreviation: "CA",
+            },
+            {
+                name: "Colorado",
+                abbreviation: "CO",
+            },
+            {
+                name: "Connecticut",
+                abbreviation: "CT",
+            },
+            {
+                name: "Delaware",
+                abbreviation: "DE",
+            },
+            {
+                name: "District Of Columbia",
+                abbreviation: "DC",
+            },
+            {
+                name: "Federated States Of Micronesia",
+                abbreviation: "FM",
+            },
+            {
+                name: "Florida",
+                abbreviation: "FL",
+            },
+            {
+                name: "Georgia",
+                abbreviation: "GA",
+            },
+            {
+                name: "Guam",
+                abbreviation: "GU",
+            },
+            {
+                name: "Hawaii",
+                abbreviation: "HI",
+            },
+            {
+                name: "Idaho",
+                abbreviation: "ID",
+            },
+            {
+                name: "Illinois",
+                abbreviation: "IL",
+            },
+            {
+                name: "Indiana",
+                abbreviation: "IN",
+            },
+            {
+                name: "Iowa",
+                abbreviation: "IA",
+            },
+            {
+                name: "Kansas",
+                abbreviation: "KS",
+            },
+            {
+                name: "Kentucky",
+                abbreviation: "KY",
+            },
+            {
+                name: "Louisiana",
+                abbreviation: "LA",
+            },
+            {
+                name: "Maine",
+                abbreviation: "ME",
+            },
+            {
+                name: "Marshall Islands",
+                abbreviation: "MH",
+            },
+            {
+                name: "Maryland",
+                abbreviation: "MD",
+            },
+            {
+                name: "Massachusetts",
+                abbreviation: "MA",
+            },
+            {
+                name: "Michigan",
+                abbreviation: "MI",
+            },
+            {
+                name: "Minnesota",
+                abbreviation: "MN",
+            },
+            {
+                name: "Mississippi",
+                abbreviation: "MS",
+            },
+            {
+                name: "Missouri",
+                abbreviation: "MO",
+            },
+            {
+                name: "Montana",
+                abbreviation: "MT",
+            },
+            {
+                name: "Nebraska",
+                abbreviation: "NE",
+            },
+            {
+                name: "Nevada",
+                abbreviation: "NV",
+            },
+            {
+                name: "New Hampshire",
+                abbreviation: "NH",
+            },
+            {
+                name: "New Jersey",
+                abbreviation: "NJ",
+            },
+            {
+                name: "New Mexico",
+                abbreviation: "NM",
+            },
+            {
+                name: "New York",
+                abbreviation: "NY",
+            },
+            {
+                name: "North Carolina",
+                abbreviation: "NC",
+            },
+            {
+                name: "North Dakota",
+                abbreviation: "ND",
+            },
+            {
+                name: "Northern Mariana Islands",
+                abbreviation: "MP",
+            },
+            {
+                name: "Ohio",
+                abbreviation: "OH",
+            },
+            {
+                name: "Oklahoma",
+                abbreviation: "OK",
+            },
+            {
+                name: "Oregon",
+                abbreviation: "OR",
+            },
+            {
+                name: "Palau",
+                abbreviation: "PW",
+            },
+            {
+                name: "Pennsylvania",
+                abbreviation: "PA",
+            },
+            {
+                name: "Puerto Rico",
+                abbreviation: "PR",
+            },
+            {
+                name: "Rhode Island",
+                abbreviation: "RI",
+            },
+            {
+                name: "South Carolina",
+                abbreviation: "SC",
+            },
+            {
+                name: "South Dakota",
+                abbreviation: "SD",
+            },
+            {
+                name: "Tennessee",
+                abbreviation: "TN",
+            },
+            {
+                name: "Texas",
+                abbreviation: "TX",
+            },
+            {
+                name: "Utah",
+                abbreviation: "UT",
+            },
+            {
+                name: "Vermont",
+                abbreviation: "VT",
+            },
+            {
+                name: "Virgin Islands",
+                abbreviation: "VI",
+            },
+            {
+                name: "Virginia",
+                abbreviation: "VA",
+            },
+            {
+                name: "Washington",
+                abbreviation: "WA",
+            },
+            {
+                name: "West Virginia",
+                abbreviation: "WV",
+            },
+            {
+                name: "Wisconsin",
+                abbreviation: "WI",
+            },
+            {
+                name: "Wyoming",
+                abbreviation: "WY",
+            },
+        ];
+        // loop - for every 'data' entry...;
+        for (i = 0; i < statesList.length; i++) {
+            // - create option tag; apply the 'province' to the value of <option>; append option to datalist '.provinceDropdown';
+            var provinceOption = $("<option></option>");
+            provinceOption.attr("value", statesList[i].name);
+            $(".provinceDropdown").append(provinceOption);
+        }
+
+var userState = "";
+// Starting with button onclick
+$("#submit-btn").on("click", function (event) { 
+  // <!-- Get user State information -->
+  userState = $("#userInput").val();
+  console.log(userState);
+  encodeURIComponent(userState);
 
   //   var searchQuery =
   //     "https://covid-19-statistics.p.rapidapi.com/provinces?iso=" + userChoice;
   //   console.log(searchQuery);
 
   var queryUrl =
-    "https://rapidapi.p.rapidapi.com/reports?iso=USA&region_name=US&city_name=" +
-    userChoice +
+    "https://rapidapi.p.rapidapi.com/reports?q=" +
+    userState +
     "&date=2020-04-16";
   console.log(queryUrl);
 
